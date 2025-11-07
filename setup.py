07284fda6c7a8197adbf65fa0b036762fb9bff3e
sizan-siz <omarrashedsizan@gmail.com>
2025-11-07 18:56:17 +0600

    requerment added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Volatility_Prediction_ML_Project",
+    version="0.0.0",
+    author="Omar Rashed Sizan",
+    author_email="omarrashedsizan313@gmail.com",
+    packages=find_packages()
+)
